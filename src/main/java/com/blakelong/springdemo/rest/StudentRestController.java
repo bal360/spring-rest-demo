dcb88ceaedcc0f734939dfaeb8f75012f8b69a03
Blake A. Long <blake.a.long@gmail.com>
2020-10-28 13:48:40 -0600

    getStudent with @PathVariable and added @PostConstruct method to handle creation of test data only once

--- src/main/java/com/blakelong/springdemo/rest/StudentRestController.java
+++ src/main/java/com/blakelong/springdemo/rest/StudentRestController.java
@@ -1,7 +1,11 @@
 package com.blakelong.springdemo.rest;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +14,31 @@ import com.blakelong.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-	
-	@RequestMapping("/students")
-	public List<Student> getStudents() {
 
-		List<Student> students = new ArrayList<Student>();
+	private List<Student> students;
+	
+	// define @PostConstruct to load the student data  ... only once!
+	@PostConstruct
+	public void loadData() {
+
+		students = new ArrayList<Student>();
 		
 		students.add(new Student("Blake", "Allan"));
 		students.add(new Student("John", "Andrews"));
 		students.add(new Student("Wesley", "Kenneth"));
 		
+	}
+	
+	
+	@RequestMapping("/students")
+	public List<Student> getStudents() {
+	
 		return students;
+	}
+	
+	@RequestMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return students.get(studentId);
 	}
 }
