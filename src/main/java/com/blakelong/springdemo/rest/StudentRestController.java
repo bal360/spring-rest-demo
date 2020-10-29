5de902307ea79ba5b5dd65c027c1a498547eca21
Blake A. Long <blake.a.long@gmail.com>
2020-10-28 20:48:33 -0600

    refactored exception handling to be global with @ControllerAdvice advice

--- src/main/java/com/blakelong/springdemo/rest/StudentRestController.java
+++ src/main/java/com/blakelong/springdemo/rest/StudentRestController.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,35 +42,6 @@ public class StudentRestController {
 		}
 		
 		return students.get(studentId);
-	}
-	
-	// add exception handler using @ExceptionHandler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
-		
-		// create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exception.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return ResponseEntity
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
-	
-	// add another exception handler - to catch any exception (catch all)
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
-		// create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exception.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return ResponseEntity
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
 	
 }
